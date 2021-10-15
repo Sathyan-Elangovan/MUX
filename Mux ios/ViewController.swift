2f43eb51690c58a1411dac3e1ffc8157ac8d5b32
Sathyan E <61688116+Sathyan-Elangovan@users.noreply.github.com>
2021-10-15 11:16:51 +0530

    initial commit

---
+++ Mux ios/ViewController.swift
@@ -0,0 +1,21 @@
+//
+        //
+        // ViewController.swift
+        // Mux ios
+        //
+        // Created by ___Sathyan Elangovan___ on 15/10/21
+       
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
